6532f568c4932b86d7ff417d78b563851bff2de9
alok160684 <alok1606@gmail.com>
2026-04-27 22:40:26 +0530

    Add base JS file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// AG Theme base JS file
+
+(function ($, Drupal) {
+  Drupal.behaviors.agTheme = {
+    attach: function (context, settings) {
+      console.log('AG Theme JS Loaded');
+    }
+  };
+})(jQuery, Drupal);
